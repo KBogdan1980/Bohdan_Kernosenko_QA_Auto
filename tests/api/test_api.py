6a50be31144818915fdc911df0bbda151b0946c2
Bohdan Kernosenko <cs300680kbp@gmail.com>
2023-08-14 23:47:51 +0300

    Created fixtures

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,35 +1,17 @@
-'''
 import pytest
 
 
-class User:
-
-    def __init__(self):
-        self.name = 'Sergii'
-        self.second_name = 'Butenko'
-
-
-@pytest.fixture
-def user():
-    yield User()
-
-
+@pytest.mark.change
 def test_remove_name(user):
     user.name = ''
     assert user.name == ''
 
 
+@pytest.mark.check
 def test_name(user):
-    assert user.name == 'Sergii'
-    
-
-def test_second_name():
-    assert user.second_name == 'Butenko'
+    assert user.name == "Sergii"
 
 
-'''
-def test_check_math():
-    assert 7 * 7 == 49
-
-def test_check_78():
-    assert 7 * 8 == 65
+@pytest.mark.check
+def test_second_name(user):
+    assert user.second_name == "Butenko"
