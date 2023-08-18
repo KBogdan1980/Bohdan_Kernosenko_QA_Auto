e44e880edb6b194203330ba2da147a585eb3b12f
Bohdan Kernosenko <cs300680kbp@gmail.com>
2023-08-18 15:12:53 +0300

    Testing HTTP

---
+++ tests/test_http.py
@@ -0,0 +1,34 @@
+import pytest
+import requests
+
+
+@pytest.mark.http
+def test_first_request():
+    r = requests.get('https://api.github.com/zen')
+    print(r.format("Response is {r.text}"))
+
+@pytest.mark.http
+def test_second_request():
+    r = requests.get('https://api.github.com/users/defunkt')
+    body = r.json()
+    headers = r.headers
+
+    assert body['name'] == 'Chris Wanstrath'
+    assert r.status_code == 200
+    assert headers['Server'] == 'GitHub.com'
+
+
+@pytest.mark.http
+def test_status_code_request():
+    r = requests.get('https://api.github.com/users/sergii_butenko')
+
+    assert r.status_code == 200 #404
+
+'''
+@pytest.mark.http
+def test_second_request():
+    r = requests.get('https://api.github.com/users/defunkt')
+    print(r.format("Response Body is {r.json()}"))
+    print(r.format("Response Status Coce is {r.status_code}"))
+    print(r.format("Response Body is {r.headers}"))
+'''
