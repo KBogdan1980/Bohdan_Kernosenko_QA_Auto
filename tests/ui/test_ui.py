6ea833ae56f971823e25845fd2c402dbc51a91e2
Bohdan Kernosenko <cs300680kbp@gmail.com>
2023-08-30 23:42:49 +0300

    Add negative test with Selenium

--- tests/ui/test_ui.py
+++ tests/ui/test_ui.py
@@ -1,5 +1,58 @@
-def test_check_math():
-    assert 7 * 7 == 49
+import pytest
 
-def test_check_78():
-    assert 7 * 8 == 65
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+@pytest.mark.ui 
+def test_check_incorrect_username():
+    #створення об'єкту для керування браузером
+    driver = webdriver.Chrome(
+        service=Service(r"/home/bohdan/Bohdan_Kernosenko_QA_Auto/" + "chromedriver")
+    )
+
+    #open the page https://github.com/login
+    driver.get("https://github.com/login")
+
+    #знаходимо поле, в яке будемо вводити неправильне ім'я користувача або поштову адресу
+    login_elem = driver.find_element(By.ID, "login_field")
+
+    #вводимо неправильне ім'я користувача або поштову адресу
+    login_elem.send_keys("bohdankernosenko@wrongemeil.com")
+
+    #знаходимо поле, в яке будемо вводити неправильний пароль
+    pass_elem = driver.find_element(By.ID, "password")
+
+    #вводимо неправильний пароль
+    pass_elem.send_keys("wrong password")
+
+    #Знаходимо кнопку sign-in
+    btn_elem = driver.find_element(By.NAME, "commit")
+
+    #емулюємо клік лівою кнопкою мишки
+    btn_elem.click()
+
+    #перевіряємо, що назва сторінки така, як ми очікуємо
+    assert driver.title == "Sign in to GitHub · GitHub"
+
+    #close the browser
+    driver.close()
+
+
+
+
+'''
+#тест щоб відкрити і закрити сторінку
+@pytest.mark.ui 
+def test_check_incorrect_username():
+    # Створення об'єкту для керування браузером
+    driver = webdriver.Chrome(
+        service=Service(r"/home/bohdan/Bohdan_Kernosenko_QA_Auto/" + "chromedriver" )
+        )
+    
+    #open the page https://github.com/login
+    driver.get("https://github.com/login")
+
+    #close the browser
+    driver.close()
+'''
